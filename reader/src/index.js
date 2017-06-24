944fb744a96d1db45cf3348580751568cc4e89af
Napol Rachatasumrit <naplr42@gmail.com>
2017-06-24 11:05:05 +0700

    Set up redux

--- reader/src/index.js
+++ reader/src/index.js
@@ -1,8 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
-import './index.css';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
+import { createStore, applyMiddleware, compose } from 'redux'
+import thunk from 'redux-thunk'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+import App from './App'
+import registerServiceWorker from './registerServiceWorker'
+import './index.css'
+import reducers from './reducers'
+
+const finalCreateStore = compose(
+    applyMiddleware(thunk)
+)(createStore)
+
+export const store = finalCreateStore(reducers)
+
+ReactDOM.render(
+  <Provider store={ store }>
+    <App />  
+  </Provider>
+  ,document.getElementById('root')
+)
+
+registerServiceWorker()
